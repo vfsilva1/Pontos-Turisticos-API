bb3f3142d3108ca673bafff0eb5fb3d385d35cb3
Vitor Silva <vfsilva5@gmail.com>
2020-03-25 09:29:01 -0300

    Query strings e django filter

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -5,3 +5,4 @@ from atracoes.api.serializers import AtracoesSerializer
 class AtracaoViewSet(viewsets.ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracoesSerializer
+    filterset_fields = ['Nome', 'Descricao']
